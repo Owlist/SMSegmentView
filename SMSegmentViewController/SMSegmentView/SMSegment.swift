f47bab964f704777fce42ff7e11a0025531ed8fc
Noah Keating <noah@quirky.com>
2016-04-06 18:34:38 -0400

    adding subtitle label

--- SMSegmentViewController/SMSegmentView/SMSegment.swift
+++ SMSegmentViewController/SMSegmentView/SMSegment.swift
@@ -51,10 +51,10 @@ public class SMSegment: SMBasicSegment {
     // Segment Title Text & Colour & Font
     public var title: String? {
         didSet {
-            self.label.text = self.title
+            self.titleLabel.text = self.title
             
-            if let titleText = self.label.text as NSString? {
-                self.labelWidth = titleText.boundingRectWithSize(CGSize(width: self.frame.size.width, height: self.frame.size.height), options:NSStringDrawingOptions.UsesLineFragmentOrigin , attributes: [NSFontAttributeName: self.label.font], context: nil).size.width
+            if let titleText = self.titleLabel.text as NSString? {
+                self.labelWidth = titleText.boundingRectWithSize(CGSize(width: self.frame.size.width, height: self.frame.size.height), options:NSStringDrawingOptions.UsesLineFragmentOrigin , attributes: [NSFontAttributeName: self.titleLabel.font], context: nil).size.width
             }
             else {
                 self.labelWidth = 0.0
@@ -63,26 +63,59 @@ public class SMSegment: SMBasicSegment {
             self.resetContentFrame()
         }
     }
+    public var subtitle: String? {
+        didSet {
+            self.subtitleLabel.text = self.subtitle
+            
+            if let subtitleText = self.subtitleLabel.text as NSString? {
+                self.labelWidth = subtitleText.boundingRectWithSize(CGSize(width: self.frame.size.width, height: self.frame.size.height), options:NSStringDrawingOptions.UsesLineFragmentOrigin , attributes: [NSFontAttributeName: self.subtitleLabel.font], context: nil).size.width
+            }
+            else {
+                self.labelWidth = 0.0
+            }
+            
+            self.resetContentFrame()
+        }
+    }
+    
     public var onSelectionTextColour: UIColor = UIColor.whiteColor() {
         didSet {
             if self.isSelected == true {
-                self.label.textColor = self.onSelectionTextColour
+                self.titleLabel.textColor = self.onSelectionTextColour
+                self.subtitleLabel.textColor = self.onSelectionTextColour
             }
         }
     }
+    
     public var offSelectionTextColour: UIColor = UIColor.darkGrayColor() {
         didSet {
             if self.isSelected == false {
-                self.label.textColor = self.offSelectionTextColour
+                self.titleLabel.textColor = self.offSelectionTextColour
+                self.subtitleLabel.textColor = self.offSelectionTextColour
             }
         }
     }
+    
     public var titleFont: UIFont = UIFont.systemFontOfSize(17.0) {
         didSet {
-            self.label.font = self.titleFont
+            self.titleLabel.font = self.titleFont
             
-            if let titleText = self.label.text as NSString? {
-                self.labelWidth = titleText.boundingRectWithSize(CGSize(width: self.frame.size.width + 1.0, height: self.frame.size.height), options:NSStringDrawingOptions.UsesLineFragmentOrigin , attributes: [NSFontAttributeName: self.label.font], context: nil).size.width
+            if let titleText = self.titleLabel.text as NSString? {
+                self.labelWidth = titleText.boundingRectWithSize(CGSize(width: self.frame.size.width + 1.0, height: self.frame.size.height), options:NSStringDrawingOptions.UsesLineFragmentOrigin , attributes: [NSFontAttributeName: self.titleLabel.font], context: nil).size.width
+            }
+            else {
+                self.labelWidth = 0.0
+            }
+            
+            self.resetContentFrame()
+        }
+    }
+    public var subtitleFont: UIFont = UIFont.systemFontOfSize(14.0) {
+        didSet {
+            self.subtitleLabel.font = self.subtitleFont
+            
+            if let titleText = self.subtitleLabel.text as NSString? {
+                self.labelWidth = titleText.boundingRectWithSize(CGSize(width: self.frame.size.width + 1.0, height: self.frame.size.height), options:NSStringDrawingOptions.UsesLineFragmentOrigin , attributes: [NSFontAttributeName: self.subtitleLabel.font], context: nil).size.width
             }
             else {
                 self.labelWidth = 0.0
@@ -116,14 +149,15 @@ public class SMSegment: SMBasicSegment {
     
    
     private var imageView: UIImageView = UIImageView()
-    private var label: UILabel = UILabel()
+    private var titleLabel: UILabel = UILabel()
+    private var subtitleLabel: UILabel = UILabel()
     private var labelWidth: CGFloat = 0.0
     
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    public init(verticalMargin: CGFloat, onSelectionColour: UIColor, offSelectionColour: UIColor, onSelectionTextColour: UIColor, offSelectionTextColour: UIColor, titleFont: UIFont) {
+    public init(verticalMargin: CGFloat, onSelectionColour: UIColor, offSelectionColour: UIColor, onSelectionTextColour: UIColor, offSelectionTextColour: UIColor, titleFont: UIFont, subtitleFont: UIFont) {
         
         self.verticalMargin = verticalMargin
         self.onSelectionColour = onSelectionColour
@@ -131,6 +165,7 @@ public class SMSegment: SMBasicSegment {
         self.onSelectionTextColour = onSelectionTextColour
         self.offSelectionTextColour = offSelectionTextColour
         self.titleFont = titleFont
+        self.subtitleFont = subtitleFont
         
         super.init(frame: CGRectZero)
         self.setupUIElements()
@@ -145,10 +180,15 @@ public class SMSegment: SMBasicSegment {
         self.imageView.contentMode = UIViewContentMode.ScaleAspectFit
         self.addSubview(self.imageView)
         
-        self.label.textAlignment = NSTextAlignment.Center
-        self.label.font = self.titleFont
-        self.label.textColor = self.offSelectionTextColour
-        self.addSubview(self.label)
+        self.titleLabel.textAlignment = NSTextAlignment.Center
+        self.titleLabel.font = self.titleFont
+        self.titleLabel.textColor = self.offSelectionTextColour
+        self.addSubview(self.titleLabel)
+        
+        self.subtitleLabel.textAlignment = NSTextAlignment.Center
+        self.subtitleLabel.font = self.subtitleFont
+        self.subtitleLabel.textColor = self.offSelectionTextColour
+        self.addSubview(self.subtitleLabel)
     }
     
     
@@ -166,16 +206,19 @@ public class SMSegment: SMBasicSegment {
         
         // If there's no text, align imageView centred
         // Else align text centred
-        if self.labelWidth == 0.0 {
+//        if self.labelWidth == 0.0 {
             imageViewFrame.origin.x = max((self.frame.size.width - imageViewFrame.size.width) / 2.0, 0.0)
-        }
-        else {
-            imageViewFrame.origin.x = max((self.frame.size.width - imageViewFrame.size.width - self.labelWidth) / 2.0 - distanceBetween, 0.0)
-        }
+//        }
+//        else {
+//            imageViewFrame.origin.x = max((self.frame.size.width - imageViewFrame.size.width - self.labelWidth) / 2.0 - distanceBetween, 0.0)
+//        }
         
         self.imageView.frame = imageViewFrame
         
-        self.label.frame = CGRectMake(imageViewFrame.origin.x + imageViewFrame.size.width + distanceBetween, self.verticalMargin, self.labelWidth, self.frame.size.height - self.verticalMargin * 2)
+        self.titleLabel.frame = CGRectMake(imageViewFrame.origin.x + imageViewFrame.size.width + distanceBetween, self.frame.size.height/4, self.labelWidth, self.frame.size.height/2)
+        
+        self.subtitleLabel.frame = CGRectMake(imageViewFrame.origin.x + imageViewFrame.size.width + distanceBetween, 0.75 * self.frame.size.height, self.labelWidth, self.frame.size.height/4)
+        
     }
     
     // MARK: Selections
@@ -183,12 +226,14 @@ public class SMSegment: SMBasicSegment {
         super.setSelected(selected, inView: view)
         if selected {
             self.backgroundColor = self.onSelectionColour
-            self.label.textColor = self.onSelectionTextColour
-            self.imageView.image = self.onSelectionImage
+            self.titleLabel.textColor = self.onSelectionTextColour
+            self.subtitleLabel.textColor = self.onSelectionTextColour
+            self.imageView.image = nil
         }
         else {
             self.backgroundColor = self.offSelectionColour
-            self.label.textColor = self.offSelectionTextColour
+            self.titleLabel.textColor = self.offSelectionTextColour
+            self.subtitleLabel.textColor = self.offSelectionTextColour
             self.imageView.image = self.offSelectionImage
         }
     }
